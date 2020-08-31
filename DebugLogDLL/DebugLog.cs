c2c00ab450db036f5bb6f11546f6643d131a6ef2
k4keye <vps32@naver.com>
2020-08-31 20:54:45 +0900

    First Commit

---
+++ DebugLogDLL/DebugLog.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Csharp_DebugLogDLL
+{
+    public class DebugLog
+    {
+        /// <summary>
+        /// Debug mode shows logcat 
+        /// Release mode doesn't show the logcat
+        /// Print out the type of data you want with the parameter.
+        /// </summary>
+        /// <param name="messages">Any data type and free count</param>
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void m(params object[] messages)
+        {
+            MessageLog(messages);
+        }
+        /// <summary>
+        /// Debug mode shows logcat 
+        /// Release mode doesn't show the logcat
+        /// Add the exception as a parameter.
+        /// </summary>
+        /// <param name="e">data type Exception</param>
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void e(Exception e)
+        {
+            ExceptionLog(e);
+        }
+
+        private static void MessageLog(params object[] messages)
+        {
+
+
+            StringBuilder result = new StringBuilder();
+            result.Append("-> ");
+            result.Append("[LogMessage]  : ");
+
+            foreach (object msssage in messages)
+            {
+                result.Append(msssage);
+                result.Append("  ");
+
+            }
+            result.Append("\n");
+            Console.WriteLine(result);
+        }
+
+
+        private static void ExceptionLog(Exception e)
+        {
+            StackTrace stackTrace = new StackTrace(true);
+            StringBuilder result = new StringBuilder();
+            short count = Convert.ToByte(stackTrace.FrameCount);
+            string spaceCount = "";
+            count = count > (short)10 ? (short)10 : (short)count;
+            for (int i = 1; i <= count - 2; i++)
+            {
+                StackFrame stackFrame = stackTrace.GetFrame(count - i);
+
+                for (int ii = 1; ii < i; ii++)
+                {
+                    spaceCount += "  ";
+                }
+                result.Append("\n");
+                result.Append(spaceCount);
+                result.Append("-> ");
+                result.Append("[ File Name ]  : ");
+                result.Append(stackFrame.GetFileName());
+                result.Append("\n");
+
+                result.Append(spaceCount);
+                result.Append("   ");
+                result.Append("[Method Name]  : ");
+                result.Append(stackFrame.GetMethod());
+                result.Append("\n");
+
+                result.Append(spaceCount);
+                result.Append("   ");
+                result.Append("[    Line   ]  : ");
+                result.Append(stackFrame.GetFileLineNumber());
+                result.Append("\n");
+
+
+            }
+
+            string eSignature = Convert.ToString(e.TargetSite);
+            string eMessage = Convert.ToString(e.Message);
+
+            result.Append(spaceCount);
+            result.Append("   ");
+            result.Append("[ Cignature ]  : ");
+            result.Append(eSignature);
+            result.Append("\n");
+
+            result.Append(spaceCount);
+            result.Append("   ");
+            result.Append("[  Message  ]  : ");
+            result.Append(eMessage);
+            result.Append("\n");
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Csharp-UtillityClassDLL
+:speech_balloon: C# an effort to catch a bug
+
+
+### :family: Support
+JJinggg(https://github.com/JJinggg)
+
+
+## DebugLog
+#
+### :eyes: Debug Mode
+show in your logcat view   
+
+
+### :ghost: Release Model
+not show in your logcat view   
+
+
+### instruction
+-----------------------------------
+![carbon](https://user-images.githubusercontent.com/52993842/91713266-63e08f80-ebc4-11ea-8654-4dbe87426ff0.png)
+
+
+### DebugLog Result
+-----------------------------------
+![carbon (1)](https://user-images.githubusercontent.com/52993842/91713363-98544b80-ebc4-11ea-8295-33d3cbff9232.png)
+
